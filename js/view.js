ce43ae757809e113145d7e77e0ec3a3d904f649b
Alexander Kostarev <UniverAlex@ya.ru>
2022-01-05 13:09:22 +0300

    css grid

--- js/view.js
+++ js/view.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const towns = document.querySelectorAll(".towns div");
+    const towns = document.querySelectorAll(".town");
+    showCreatures("castle");
     towns.forEach(town => town.onclick = (event) => {
         towns.forEach(town => town.classList.remove("townSelection"));
         event.target.classList.add("townSelection")
@@ -11,16 +12,16 @@ function showCreatures(town) {
     const townCreatures = getCreatures(town);
     // TODO: Попробуй сделать генератор, который возвращает существ по очереди.
     const levels = document.querySelectorAll(".creatures .creature");
-    townCreatures.forEach((oCreature, idx)=> {
+    townCreatures.forEach((oCreature, idx) => {
 
     });
     levels.forEach((level, idx) => {
         if (townCreatures[idx]) {
-            level.children[0].style.background = "url(img/creatures/" + town + "/" + townCreatures[idx][0].name + ".gif)" + "no-repeat";
-            level.children[1].style.background = "url(img/creatures/" + town + "/" + townCreatures[idx][1].name + ".gif)" + "no-repeat";
+            level.children[0].style.backgroundImage = "url(img/creatures/" + town + "/" + townCreatures[idx][0].name + ".gif)";
+            level.children[1].style.backgroundImage = "url(img/creatures/" + town + "/" + townCreatures[idx][1].name + ".gif)";
         } else {
-            level.children[0].style.background = "";
-            level.children[1].style.background = "";
+            level.children[0].style.backgroundImage = "";
+            level.children[1].style.backgroundImage = "";
         }
     });
 }
