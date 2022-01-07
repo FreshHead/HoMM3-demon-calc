887af2dd0fd87b631007e605f989cfd287b50666
Alexander Kostarev <UniverAlex@ya.ru>
2022-01-07 08:17:47 +0300

    add all town creatures

--- js/view.js
+++ js/view.js
@@ -7,9 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
         this.showCreatures(event.target.id);
     });
 });
-
+const townCreatures = {
+    castle: [10, 10, 25, 35, 40],
+    conflux: [3],
+    cove: [4, 15, 30, 35],
+    dungeon: [5, 6, 14, 22, 25, 30, 50],
+    fortress: [6, 14, 15, 20, 35],
+    inferno: [4, 13, 25],
+    neutral: [1, 4, 10, 20, 15, 30, 35, 15, 40, 50, 30],
+    rampart: [8, 10, 20, 15, 30, 55],
+    stronghold: [5, 10, 15, 20, 40],
+    tower: [4, 25, 30, 40]
+};
 function showCreatures(town) {
-    const creatures = getCreatures(town);
+    const creatures = townCreatures[town];
     const creaturesDiv = document.getElementById("creatureList");
     creaturesDiv.textContent = ""; // Remove all old children.
     let offset = 0;
@@ -20,12 +31,6 @@ function showCreatures(town) {
         creatureDiv.style.backgroundPositionX = offset + "px";
         creaturesDiv.append(creatureDiv);
         offset -= 58;
-        if (creature[1]) {
-            const upgradedCreatureDiv = document.createElement("div");
-            upgradedCreatureDiv.style.backgroundImage = "url(img/sprite_sheets/)" + town + "_creatures.png)";
-            creaturesDiv.append(upgradedCreatureDiv);
-            offset += 58;
-        }
     });
     // TODO: Попробуй сделать генератор, который возвращает существ по очереди.
 }
