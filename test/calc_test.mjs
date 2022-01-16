14d24d94d6deaf0edb10b079767cbd838fd829f2
Alexander Kostarev <UniverAlex@ya.ru>
2022-01-16 10:06:15 +0300

    add scipts into package.json

--- test/calc_test.mjs
+++ test/calc_test.mjs
@@ -4,8 +4,8 @@ import assert from "assert/strict";
 
 { // getDemonsNumberTest block.
     const tests = [
-        [[3, 15, 40], 1, "3 pitlords and 15 imps"],
-        [[25, 100, 40], 11, "25 pitlords and 100 gremlins"] // example from https://homm3sod.ru/demonologiya/.
+        [[3, 15, 4], 1, "3 pitlords and 15 imps"],
+        [[25, 100, 4], 11, "25 pitlords and 100 gremlins"] // example from https://homm3sod.ru/demonologiya/.
     ];
 
     for (const test of tests) {
